9ff50f003dd48d6cc52fe19ecf4d5ca87ca6442f
xqrzd <4950876+xqrzd@users.noreply.github.com>
2020-06-01 22:15:50 -0400

    Initial check-in of Kudu 1.12

---
+++ src/Knet.Kudu.Binary/ExecutableInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Knet.Kudu.Binary
+{
+    public class ExecutableInfo
+    {
+        /// <summary>
+        /// Path to the executable.
+        /// </summary>
+        public string ExePath { get; }
+
+        /// <summary>
+        /// Any environment variables that should be set when running the executable.
+        /// </summary>
+        public IReadOnlyDictionary<string, string> EnvironmentVariables { get; }
+
+        public ExecutableInfo(string exePath, IReadOnlyDictionary<string, string> environmentVariables)
+        {
+            ExePath = exePath;
+            EnvironmentVariables = environmentVariables;
+        }
+    }
+}
